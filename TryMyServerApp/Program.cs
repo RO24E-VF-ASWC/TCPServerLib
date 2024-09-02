49020280bd79f066660baacdfeca9cb461409093
Peter Levinsky <pele@zealand.dk>
2024-09-02 13:35:58 +0200

    Add Configuration

--- TryMyServerApp/Program.cs
+++ TryMyServerApp/Program.cs
@@ -3,14 +3,17 @@
 using TryMyServerApp.div;
 using TryMyServerApp.Server;
 
-MyServer server = new MyServer();
-server.Start();
+//MyServer server = new MyServer();
+//server.Start();
 
 //Regn regn = new Regn();
 
 //int res = regn.Div(8,3);
 //Console.WriteLine(res);
 
+
+ReadConfig conf = new ReadConfig();
+conf.Read("../../../div/Config.xml");
 
 Console.WriteLine("press any key");
 Console.ReadKey();
--- TryMyServerApp/div/ReadConfig.cs
+++ TryMyServerApp/div/ReadConfig.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace TryMyServerApp.div
 {
@@ -11,6 +12,23 @@ namespace TryMyServerApp.div
 
         public void Read(string filename)
         {
+            XmlDocument configDoc = new XmlDocument(); 
+            configDoc.Load(filename);
+
+
+            XmlNode portNode = configDoc.DocumentElement.SelectSingleNode("Port");
+            if (portNode != null) 
+            { 
+                String portStr = portNode.InnerText.Trim(); 
+                int port = Convert.ToInt32(portStr); 
+            }
+
+
+            XmlNode nameNode = configDoc.DocumentElement.SelectSingleNode("Name");
+            if (nameNode != null)
+            {
+                String name = nameNode.InnerText.Trim();
+            }
 
 
         }
