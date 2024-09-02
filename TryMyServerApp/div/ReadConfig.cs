e92777653eeb58e0394467a7a6cd29f3260f5ba7
Peter Levinsky <pele@zealand.dk>
2024-09-02 12:49:03 +0200

    Igen

---
+++ TryMyServerApp/div/ReadConfig.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryMyServerApp.div
+{
+    public class ReadConfig
+    {
+
+        public void Read(string filename)
+        {
+
+
+        }
+    }
+}
